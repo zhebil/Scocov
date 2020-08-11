2fe6aa64f9925906966a46bad604f641170817aa
zhebil <jbilyk1996@gmail.com>
2020-08-11 07:33:03 +0300

    todo commit

---
+++ todo/precache-manifest.0ff0b821fe3e4dfbb358434fd525af5a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91aaedb30e5e10dff96d9fed468aa61e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6cdebd7b22dd02849532",
+    "url": "/static/css/main.de55f840.chunk.css"
+  },
+  {
+    "revision": "be19641d3fe78d35223f",
+    "url": "/static/js/2.ef8b4283.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ef8b4283.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6cdebd7b22dd02849532",
+    "url": "/static/js/main.ed7d3fea.chunk.js"
+  },
+  {
+    "revision": "559ff28579f192bdf8b9",
+    "url": "/static/js/runtime-main.98bec8e4.js"
+  }
+]);
